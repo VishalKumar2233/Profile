829f382b7eebde8055c0601a5e4fa2df8ef180b1
VishalKumar2233 <113540769+VishalKumar2233@users.noreply.github.com>
2022-09-29 21:25:08 +0530

    Profile

---
+++ src/model/Product.java
@@ -0,0 +1,187 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hello
+ */
+public class Product {
+    
+    private String firstname;
+    private String lastname;
+    private String dob;
+    private String age;
+    private String telephonenumber;
+    private String email;
+    private String streetlinea;
+    private String streetlineb;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String degreea;
+    private String degreeastart;
+    private String degreeaend;
+    private String degreeb;
+    private String degreebstart;
+    private String degreebend;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(String telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreetlinea() {
+        return streetlinea;
+    }
+
+    public void setStreetlinea(String streetlinea) {
+        this.streetlinea = streetlinea;
+    }
+
+    public String getStreetlineb() {
+        return streetlineb;
+    }
+
+    public void setStreetlineb(String streetlineb) {
+        this.streetlineb = streetlineb;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getDegreea() {
+        return degreea;
+    }
+
+    public void setDegreea(String degreea) {
+        this.degreea = degreea;
+    }
+
+    public String getDegreeastart() {
+        return degreeastart;
+    }
+
+    public void setDegreeastart(String degreeastart) {
+        this.degreeastart = degreeastart;
+    }
+
+    public String getDegreeaend() {
+        return degreeaend;
+    }
+
+    public void setDegreeaend(String degreeaend) {
+        this.degreeaend = degreeaend;
+    }
+
+    public String getDegreeb() {
+        return degreeb;
+    }
+
+    public void setDegreeb(String degreeb) {
+        this.degreeb = degreeb;
+    }
+
+    public String getDegreebstart() {
+        return degreebstart;
+    }
+
+    public void setDegreebstart(String degreebstart) {
+        this.degreebstart = degreebstart;
+    }
+
+    public String getDegreebend() {
+        return degreebend;
+    }
+
+    public void setDegreebend(String degreebend) {
+        this.degreebend = degreebend;
+    }
+    
+    
+    
+}
